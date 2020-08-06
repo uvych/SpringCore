5bc5724f61e2b3f9f2d96558ecb51c83a656fdbe
Georgy <georgy143w@gmail.com>
2020-08-06 19:01:14 +0300

    Added homework

--- src/main/java/com/gb/vtb/springcore/App.java
+++ src/main/java/com/gb/vtb/springcore/App.java
@@ -1,8 +1,20 @@
 package com.gb.vtb.springcore;
 
 
+import com.gb.vtb.springcore.config.Config;
+import com.gb.vtb.springcore.config.PrepareDataApp;
+import com.gb.vtb.springcore.service.ProductService;
+import com.gb.vtb.springcore.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App {
     public static void main(String[] args) {
+        PrepareDataApp.forcePrepareData();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        UserService userService = context.getBean("userService", UserService.class);
+        ProductService productService = context.getBean("productService", ProductService.class);
 
+        System.out.println(userService.getAllUsers());
+        System.out.println(productService.getAllProduct());
     }
 }
